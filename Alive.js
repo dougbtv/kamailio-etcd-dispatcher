3d953d8314d261fe2f6feeed1bc065f7e592515f
dougbtv <douglaskippsmith@gmail.com>
2015-10-23 09:52:43 -0400

    [error handling] updates error handling to determine if etcd is alive when process is started

---
+++ Alive.js
@@ -0,0 +1,50 @@
+// We need to see if the etcd host if alive at all...
+module.exports = function(log,opts) {
+
+	var http = require('http');
+
+	var httpoptions = {
+		hostname: opts.etcdhost,
+		port: opts.etcdport,
+		path: '/version',
+		method: 'GET',
+		headers: { 'Content-Type': 'application/json' }
+	};
+
+	this.isalive = function(callback) {
+
+		var req = http.request(httpoptions, function(res) {
+
+			// res.setEncoding('utf8');
+
+			res.on('data', function (data) {
+				var version;
+
+				try {
+					version = JSON.parse(data);
+				} catch (e) {
+					log.error('json_parse_etcd_version',{ note: 'etcd version did not parse', error: e});
+					callback(e);
+				}
+
+				console.log(version);
+				if (version.etcdserver.match(/\d\.\d\.\d/)) {
+					// Looks like we have a good version.
+					log.it('etcd_alive',{version: version});
+					callback(false);
+				}
+			});
+
+		});
+
+		req.on('error', function(e) {
+			log.error('check_etcd_alive',{ note: 'could not see that etcd is alive', error: e, httpoptions: httpoptions});
+			callback(e);
+		});
+		req.end();
+
+
+	}
+
+
+}
--- test/regression.js
+++ test/regression.js
@@ -10,6 +10,23 @@ var client = restify.createJsonClient({
 
 */
 
+/*
+
+https://coreos.com/etcd/docs/latest/docker_guide.html
+
+docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 \
+ --name etcd quay.io/coreos/etcd:latest \
+ -name etcd0 \
+ -advertise-client-urls http://127.0.0.1:2379,http://127.0.0.1:4001 \
+ -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
+ -initial-advertise-peer-urls http://127.0.0.1:2380 \
+ -listen-peer-urls http://0.0.0.0:2380 \
+ -initial-cluster-token etcd-cluster-1 \
+ -initial-cluster etcd0=http://127.0.0.1:2380 \
+ -initial-cluster-state new
+
+*/
+
 var fork = require('child_process').fork;
 
 // Start up app.
